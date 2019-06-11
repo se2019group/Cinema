c730f2e1fc513a89bcd55091b0e6967b6e1e7798
杨潇 <1749295035@qq.com>
2019-06-11 11:41:56 +0800

    完善评价系统

---
+++ src/main/java/com/example/cinema/vo/MarkRecordForm.java
@@ -0,0 +1,34 @@
+package com.example.cinema.vo;
+
+public class MarkRecordVO {
+
+    private int userId;
+
+    private double mark;
+
+    private int movieId;
+
+    private int ticketId;
+
+    private String comment;
+
+    public int getUserId(){ return userId;}
+
+    public void setUserId(int userId){ this.userId=userId;}
+
+    public int getMovieId(){ return movieId;}
+
+    public void setMovieId(int movieId){ this.movieId=movieId;}
+
+    public int getTicketId(){ return ticketId;}
+
+    public void setTicketId(int ticketId){ this.ticketId=ticketId;}
+
+    public double getMark(){ return mark;}
+
+    public void setMark(double mark){ this.mark=mark;}
+
+    public String getComment(){ return comment;}
+
+    public void setComment(String comment){ this.comment=comment;}
+}
